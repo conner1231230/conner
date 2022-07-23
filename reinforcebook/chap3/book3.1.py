ca2e51623351c56b3e00eddbd1f88bc97f2d52f0
conner1231230 <94916111+conner1231230@users.noreply.github.com>
2022-07-24 01:44:13 +0800

    Create book3.1.py

---
+++ reinforcebook/chap3/book3.1.py
@@ -0,0 +1,75 @@
+import random
+import numpy as np
+
+L = 4
+D = 3
+Sg = [2,L-1]
+So = [2,1]
+
+#parameter setting
+myalpha = 0.1
+eps = 0.1
+mygamma = 0.1
+
+#initialization
+tempQ = np.zeros((D,L,4))
+
+#Function
+def eps_greedy(Q,S,eps):
+    if random.random() < eps:
+        A = random.randint(0,3)
+    else:
+        A = Q[S[0]][S[1]][:].argmax()
+
+    return A
+
+#Gridworld Environment
+def env_SW(S,A,L,D):
+    #state transition
+    if A == 1 and S[0]-1 >= 0:#up
+        S[0] = S[0]-1
+    elif A == 2 and S[1]+1 < L:#right
+        S[1] = S[1]+1
+    elif A == 3 and S[0]+1 < D:#down
+        S[0] = S[0]+1
+    elif A == 4 and S[1]-1 >= 0:#left
+        S[1] = S[1]-1
+    #reward setting
+    if S[0] == 1:
+        R=-100
+    else:
+        R=-1
+    return [S,R]
+
+#Monte Carlo Control 
+Q=tempQ
+times=100
+while(times>0):
+    S=So
+    S_queue=np.array([0,0])
+    R_queue=np.array([])
+    A_queue=np.array([])
+
+    while S!=Sg:
+        A=eps_greedy(Q,S,eps)
+        S_queue = np.vstack((S_queue,S))
+        A_queue = np.append(A_queue,A)
+        [S,R] = env_SW(S,A,L,D)
+        R_queue = np.append(R_queue,R)
+
+    T=R_queue.size-1
+    G=0
+
+    for t in range(T,-1,-1):
+        G=R_queue[t]+mygamma*G
+        Q[[int(S_queue[t+1,0])],[int(S_queue[t+1,1])],[int(A_queue[t])]]+=myalpha*(G-Q[[int(S_queue[t+1,0])],[int(S_queue[t+1,1])],[int(A_queue[t])]])
+    times-=1
+print(Q)
+
+
+
+
+
+
+
+
