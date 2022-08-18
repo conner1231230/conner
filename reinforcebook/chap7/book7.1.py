cf495142bca0561c5e5528c42e16700e60e102c7
conner1231230 <94916111+conner1231230@users.noreply.github.com>
2022-08-18 22:31:29 +0800

    Update book7.1.py

--- reinforcebook/chap7/book7.1.py
+++ reinforcebook/chap7/book7.1.py
@@ -1,6 +1,7 @@
+from audioop import avg
 import random
 import numpy as np
-import sys
+import matplotlib.pyplot as plt
 
 L = 10
 D = 5
@@ -15,32 +16,33 @@ n = 4
 #initialization
 tempQ = np.random.randn(D, L, 4)
 tempQ[Sg[0]][Sg[1]][:] = 0
-
+#tempQ = np.zeros((D,L,4))
 #Function
 
 def Ex7_1_env(S, A, L, D):
-    Sc = S
+    Sc = S.copy()
+    temp =S.copy()
     #state transition
     if A == 0 and S[0]-1 >= 0: #up
-        S[0] = S[0] - 1
+        temp[0] = S[0] - 1
     if A == 1 and S[1]+1 < L: #right
-        S[1] = S[1] + 1
+        temp[1] = S[1] + 1
     if A == 2 and S[0]+1 < D: #down
-        S[0] = S[0] + 1
+        temp[0] = S[0] + 1
     if A == 3 and S[1]-1 >= 0: #left
-        S[1] = S[1] - 1
+        temp[1] = S[1] - 1
     #block detection
-    if S[0] >= 1 and S[0] <= 3 and S[1] == 3:
-        S = Sc
-    if S[0] >= 2 and S[0] <= 5 and S[1] == 7:
-        S = Sc
+    if temp[0] >= 1 and temp[0] <= 3 and temp[1] == 3:
+        temp = Sc
+    if temp[0] >= 2 and temp[0] <= 4 and temp[1] == 6:
+        temp = Sc
     #reward setting
-    if S[0] == 0:
+    if temp[0] == 0:
         R = -100
     else:
         R = -1
 
-    return [S, R]
+    return [temp, R]
 
 def eps_greedy(Q, S, eps):
     A = np.argmax(Q[S[0]][S[1]][:])
@@ -60,35 +62,61 @@ def find(Model_visit):
     return templist
 
 #Dyna-Q
-times = 100
 Q = tempQ.copy()
-Model = np.zeros((D, L, 4, 3))
-Model_visit = np.zeros((D, L, 4))
-steplist =[]
-while times > 0:
-    step = 0
-    S_prime = So.copy()
-    while S_prime != Sg:
-        S = S_prime.copy()
-        A = eps_greedy(Q, S, eps)
-        [S_prime, R] = Ex7_1_env(S, A, L, D)
-        #reward += R
-        Q[S[0]][S[1]][A] =Q[S[0]][S[1]][A] + myalpha*(R+max(Q[S_prime[0]][S_prime[1]][:])-Q[S[0]][S[1]][A])
-        Model_visit[S[0]][S[1]][A] = 1 #record visited pairs
-        Model[S[0]][S[1]][A][0] = R
-        Model[S[0]][S[1]][A][1] = S_prime[0]
-        Model[S[0]][S[1]][A][2] = S_prime[1]
-        for k in range(n):
-            #find visited state-action pairs
-            Ind_visit = find(Model_visit)
-            Ind_select = random.randint(0,len(Ind_visit)-1)
-            temp = Model[Ind_visit[Ind_select][0]][Ind_visit[Ind_select][1]][Ind_visit[Ind_select][2]]
-            R = temp[0]
-            S_double_prime = [int(temp[1]), int(temp[2])]
-            Q[S[0]][S[1]][A] =Q[S[0]][S[1]][A]+ myalpha*(R+max(Q[S_double_prime[0]][S_double_prime[1]][:])-Q[S[0]][S[1]][A])
-        step += 1
-    steplist.append(step)
-    times -= 1
-print(steplist)
-np.set_printoptions(linewidth=190,threshold=sys.maxsize)
-print(Q)
+
+avgtime = 10
+avgsteplist = []
+while avgtime > 0:
+    times = 80
+    steplist =[]
+
+    while times > 0:
+        Model = np.zeros((D, L, 4, 3))
+        Model_visit = np.zeros((D, L, 4))
+        step = 0
+        S_prime = So.copy()
+        while S_prime != Sg:
+            S = S_prime.copy()
+            A = eps_greedy(Q, S, eps)
+            [S_prime, R] = Ex7_1_env(S, A, L, D)
+            #reward += R
+            Q[S[0]][S[1]][A] =Q[S[0]][S[1]][A] + myalpha*(R+np.max(Q[S_prime[0]][S_prime[1]][:])-Q[S[0]][S[1]][A])
+            Model_visit[S[0]][S[1]][A] = 1 #record visited pairs
+            Model[S[0]][S[1]][A][0] = R
+            Model[S[0]][S[1]][A][1] = S_prime[0]
+            Model[S[0]][S[1]][A][2] = S_prime[1]
+            step += R
+            for k in range(n):
+                #find visited state-action pairs
+                #Ind_visit = find(Model_visit)
+                #Ind_select = random.randint(0,len(Ind_visit)-1)
+                while True:
+                    tempi = random.randint(0,D-1)
+                    tempj = random.randint(0,L-1)
+                    tempp = random.randint(0,3)
+                    if Model_visit[tempi][tempj][tempp] == 1:
+                        break
+                temp = Model[tempi][tempj][tempp]
+                #temp = Model[Ind_visit[Ind_select][0]][Ind_visit[Ind_select][1]][Ind_visit[Ind_select][2]]
+                R = temp[0]
+                S_double_prime = [int(temp[1]), int(temp[2])]
+                Q[tempi][tempj][tempp] =Q[tempi][tempj][tempp]+ myalpha*(R+np.max(Q[S_double_prime[0]][S_double_prime[1]][:])-Q[tempi][tempj][tempp])
+        steplist.append(step)
+        times -= 1
+    avgsteplist.append(steplist)
+    avgtime -= 1
+    print(avgtime)
+ans = []
+for i in range(len(avgsteplist[0])):
+    temp = 0
+    for j in range(5):
+        temp += avgsteplist[j][i]
+    ans.append(temp/5)
+
+x = np.linspace(0,len(ans)-1,len(ans))
+plt.plot(x,ans,label='Normal weight')
+
+plt.legend()
+plt.show()
+#np.set_printoptions(linewidth=190,threshold=sys.maxsize)
+#print(Q)
