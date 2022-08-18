0ec35807bc9384b24acaa06d0b11766fba6ab7dc
conner1231230 <94916111+conner1231230@users.noreply.github.com>
2022-08-18 21:13:11 +0800

    Update book4.1.py

--- reinforcebook/chap4/book4.1.py
+++ reinforcebook/chap4/book4.1.py
@@ -1,6 +1,7 @@
+from audioop import avg
 import random
 import numpy as np
-
+import matplotlib.pyplot as plt
 L = 10
 D = 5
 So = [1 , 0]
@@ -28,51 +29,89 @@ def eps_greedy(Q, S, eps):
 
 #gridworld environment
 def env_SW(S, A, L, D):
+    S_prime=S.copy()
     #state transition
     if A == 0 and S[0]-1 >= 0:#up
-        S[0] = S[0] - 1
+        S_prime[0] = S[0] - 1
     if A == 1 and S[1]+1 < L:#right
-        S[1] = S[1] + 1
+        S_prime[1] = S[1] + 1
     if A == 2 and S[0]+1 < D:#down
-        S[0] = S[0] + 1
+        S_prime[0] = S[0] + 1
     if A == 3 and S[1] - 1 >= 0:#left
-        S[1] = S[1] - 1
+        S_prime[1] = S[1] - 1
     #reward setting
-    if S[0] == 0:
+    if S_prime[0] == 0:
         R=-100
     else:
         R=-1
 
-    return [S, R]
+    return [S_prime, R]
 
-
+"""
 #Sarsa
-Q = tempQ
-i=3000
-while i>0:
-    S = So.copy()
-    A = eps_greedy(Q, S, eps)
-    while S != Sg:
-        [S_prime, R] = env_SW(S, A, L, D)
-        A_prime = eps_greedy(Q, S_prime, eps)
-        Q[S[0]][S[1]][A] = Q[S[0]][S[1]][A] + myalpha*(R+ garmma* Q[S_prime[0]][S_prime[1]][A_prime]- Q[S[0]][S[1]][A])
-        S = S_prime
-        A = A_prime
-    i-=1
-np.set_printoptions(suppress=True)
-print(Q)
+Q = np.zeros((D, L, 4))
+avgtimes = 40
+avgsteplist = []
+while avgtimes > 0:
+    i=300
+    steplist = []
+    while i>0:
+        avgR = 0
+        S = So.copy()
+        A = eps_greedy(Q, S, eps)
+        while S != Sg:
+            [S_prime, R] = env_SW(S, A, L, D)
+            avgR += R
+            A_prime = eps_greedy(Q, S_prime, eps)
+            Q[S[0]][S[1]][A] = Q[S[0]][S[1]][A] + myalpha*(R+ garmma* (Q[S_prime[0]][S_prime[1]][A_prime])- Q[S[0]][S[1]][A])
+            S = S_prime
+            A = A_prime
+        steplist.append(avgR)
+        i-=1
+    avgsteplist.append(steplist)
+    avgtimes -= 1
+ans = []
 
+for i in range(len(avgsteplist[0])):
+    temp = 0
+    for j in range(10):
+        temp += avgsteplist[j][i]
+    ans.append(temp/10)
+
+"""
 #Q-learning
-Q = tempQ
-i=3000
-while i>0:
-    S = So
-    A = eps_greedy(Q, S, eps)
-    while S != Sg:
-        [S_prime, R] = env_SW(S, A, L, D)
-        A_prime = eps_greedy(Q, S_prime, eps)
-        Q[S[0]][S[1]][A] = Q[S[0]][S[1]][A] + myalpha*(R+ garmma* np.max(Q[S_prime[0]][S_prime[1]][:])- Q[S[0]][S[1]][A])
-        S = S_prime
-        A = A_prime
-    i-=1
-print(Q)
+Q = np.zeros((D, L, 4))
+avgtimes = 40
+avgsteplist = []
+while avgtimes > 0:
+    i=300
+    steplist = []
+    while i>0:
+        avgR = 0
+        S = So.copy()
+        A = eps_greedy(Q, S, eps)
+        while S != Sg:
+            [S_prime, R] = env_SW(S, A, L, D)
+            avgR += R
+            A_prime = eps_greedy(Q, S_prime, eps)
+            Q[S[0]][S[1]][A] = Q[S[0]][S[1]][A] + myalpha*(R+ garmma* np.max(Q[S_prime[0]][S_prime[1]][:])- Q[S[0]][S[1]][A])
+            S = S_prime
+            A = A_prime
+        steplist.append(avgR)
+        i-=1
+    avgsteplist.append(steplist)
+    avgtimes -= 1
+ans = []
+
+for i in range(len(avgsteplist[0])):
+    temp = 0
+    for j in range(10):
+        temp += avgsteplist[j][i]
+    ans.append(temp/10)
+
+print(ans)
+x = np.linspace(0,len(ans)-1,len(ans))
+plt.plot(x,ans,label='Normal weight')
+
+plt.legend()
+plt.show()
